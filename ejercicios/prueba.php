f41947696ae5e1de485f1dda153eed00a54e8c4c
gaonispildoras <srgaona1@gmail.com>
2021-01-29 00:24:14 +0100

    nuevo

--- ejercicios/prueba.php
+++ ejercicios/prueba.php
@@ -1,8 +1,33 @@
+<H1>INTENTO 1</H1>
 <?php
-$prueba = $_FILES["archivo"]["tmp_name"];
-echo $prueba;
+$prueba = $_FILES["archivo"]["name"];
+$prueba2 = $_FILES["archivo"]["tmp_name"];
+echo $prueba; echo "<br>";
+echo $prueba2;
 if (isset($_POST["enviar"])){
     unlink($_FILES["archivo"]["tmp_name"]);
+    
+}
+
+?>
+
+
+<form action="<?php $_SERVER['PHP_SELF'];?>"  method="post" enctype="multipart/form-data">
+    <input type="file" name="archivo" >
+    <input type="submit" name="enviar">
+</form>
+
+
+
+
+<H1>INTENTO 2</H1>
+<?php
+$nombre_f = $_FILES["archivo"]["name"];
+$ruta_f = $_FILES["archivo"]["tmp_name"];
+echo $prueba; echo "<br>";
+echo $prueba2;
+if (isset($_POST["enviar"])){
+    unlink("carpeta_archivos/".$nombre_f);
     
 }
 
